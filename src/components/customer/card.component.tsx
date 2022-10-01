ab0b4c02a2039e21919558b676472e832195c14c
Alain RAMBELOSON <alain.rambeloson@gmail.com>
2022-10-01 09:15:47 +0200

    feat: Now the card component displays the vehicle conditionnaly

--- src/components/customer/card.component.tsx
+++ src/components/customer/card.component.tsx
@@ -1,8 +1,14 @@
+import { Match, Switch } from "solid-js";
+import Entity from "../../enum/entity.enum";
+
 export default function Card(props: any) {
   return (
     <>
-      <div>Card Component</div>
-      <div>Vehicle {props.entity}</div>
+      <Switch>
+        <Match when={props.type == Entity.Vehicle}>
+          <div>Card Component Vehicle {props.entity}</div>
+        </Match>
+      </Switch>
     </>
   );
 }
